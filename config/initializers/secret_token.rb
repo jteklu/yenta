2cee301f26b9a116aa73a64f656c36da3f6be1af
Jonathan <jteklu1@umbc.edu>
2015-09-30 21:14:29 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yenta::Application.config.secret_key_base = 'a37029cb1f946f6713babfaf260bf578500273298d6fad3f911b4a133bb32c2d5f05b8cf6f5afa0d6f94d42ba72e54bc3fe8d8ec94954a1c7e1646efd8825d8e'

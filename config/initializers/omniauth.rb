c33744097910da15289a0f153142eb6872324d96
Jonathan <jteklu1@umbc.edu>
2015-10-03 00:10:24 +0000

    Adjusted application.css.scss and steyles sheets.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do |config|
-  	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:scope => 'user_about_me, user_birthday, user_location, email'}	    #required
+  	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:scope => 'user_about_me, user_birthday, user_location, email'}	#required
 end
